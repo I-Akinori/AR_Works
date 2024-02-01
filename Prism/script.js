948c773368aadf0a4eed1c2ff6284b20402b760d
I-Akinori <rhaarle1140@gmail.com>
2024-02-01 11:17:08 +0900

    Modify prism

--- Prism/script.js
+++ Prism/script.js
@@ -1,65 +1,8 @@
-const meshes = [];
-const now = new Date();
-const normalizedDay = normalizeDate(now);
-const rand_dic = [];
-
-let tree_wid_ave;
-let tree_wid_amp;
-let tree_len_ave;
-let tree_len_amp;
-let tree_dir_amp;
-let tree_ang_ave;
-let tree_ang_amp;
-let tree_col_amp;
-let tree_col_rat;
-
-let branchID = 0;
-let randID = 0;
 let count = 0;
-const Balls = [];
 const Prisms = [];
-InitRand();
+let totalheight = 0;
 var group = new THREE.Group();
 
-function InitRand() { 
-    for (let i = 0; i < 1024 * 2; i++) { 
-        rand_dic.push(Math.random());
-    }
-}
-function MyRandom() { 
-    return rand_dic[randID++];
-}
-function deg2rad(deg) { 
-    return deg / 180 * Math.PI;
-}
-function normalizeDate(date) {
-    const startOfYear = new Date(date.getFullYear(), 0, 1);
-    const endOfYear = new Date(date.getFullYear(), 11, 31);
-    const totalDays = (endOfYear - startOfYear) / (1000 * 60 * 60 * 24);
-    const dayOfYear = (date - startOfYear) / (1000 * 60 * 60 * 24);
-
-    return dayOfYear / totalDays;
-}
-function boxMuller01(ave) {
-    let u = 0, v = 0;
-    while (u === 0) u = MyRandom();
-    while (v === 0) v = MyRandom();
-    
-    let num = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v) * 0.5 + ave;
-    if (num < 1) { num = 1; }
-    return num;
-}
-function randomColor(orig) { 
-    const color = new THREE.Color();
-    color.setHSL(boxMuller01(0.7 - normalizedDay), 1.0, 0.5);
-    let colVec = new THREE.Vector3(color.r, color.g, color.b);
-    return colVec.lerp(orig, MyRandom() * tree_col_rat);
-}
-function randomColor() {
-    const color = new THREE.Color();
-    color.setHSL(Math.random(), 1.0, 0.7);
-    return color;
-}
 function rainbow(val) { 
     const color = new THREE.Color();
     if (val < 0.25) {
@@ -69,7 +12,7 @@ function rainbow(val) {
     } else if (val < 0.50) {
         color.r = 0;
         color.g = 1;
-        color.b = (val - 0.25) * 4;
+        color.b = (0.5 - val) * 4;
     } else if (val < 0.75) {
         color.r = (val - 0.5) * 4;
         color.g = 1;
@@ -94,41 +37,22 @@ function prism(x, z) {
     group.add(mesh);
     return mesh;
 }
-class Ball{
-    constructor(){
-        this.start_time = performance.now();
-        this.mesh = ball();
-        this.vel = boxMuller01(2);
-    }
-    updatePosition() {
-        const sec = (performance.now() - this.start_time) / 1000;
-        this.mesh.position.y = sec * 5 * this.vel - 0.5;
-        this.mesh.position.z = sec * sec * 30;
-        this.mesh.rotation.x = sec * 10;
-        console.log(sec);
 
-        return sec > 5;
-    }
-    dispose() { 
-        if (this.mesh.geometry) {
-            this.mesh.geometry.dispose();
-        }
-        if (this.mesh.material) {
-            this.mesh.material.dispose();
-        }
-    }
-}
 class Prism { 
     constructor(x, z, h) {
         this.height = h;
+        this.height_pre = h;
+        this.height_pre2 = h;
         this.mesh = prism(x, z);
         this.updateHeight();
+        totalheight += h;
     }
     updateHeight() {
         this.mesh.scale.y = this.height;
         this.mesh.position.y = (this.height / 2) * 0.25;
-        console.log(this.mesh.position.y);
         this.mesh.material.color = rainbow(this.height / 6.2);
+        this.height_pre2 = this.height_pre;
+        this.height_pre = this.height;
     }
     dispose() {
         if (this.mesh.geometry) {
@@ -193,7 +117,8 @@ document.body.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
 scene.visible = false;
-const camera = new THREE.OrthographicCamera(-1, +1, height / width, -height / width, 0.01, 1000);
+//const camera = new THREE.OrthographicCamera(-1, +1, height / width, -height / width, 0.01, 1000);
+const camera = new THREE.PerspectiveCamera();
 scene.add(camera);
 
 //pannel();
@@ -201,7 +126,7 @@ for (i = 0; i < 5; i++) {
     for (j = 0; j < 6; j++) {
         const x = 1.25 / 5 * (- 2 + i);
         const y = 1.5 / 6 * (- 2.5 + j);
-        const p = new Prism(x, y, (i + j + 1) * 0.3);
+        const p = new Prism(x, y, (i + j + 3) * 0.4);
         Prisms.push(p);
     }
 }
@@ -240,11 +165,18 @@ arToolkitContext.init(() => {
 
 const arMarkerControls = new THREEx.ArMarkerControls(arToolkitContext, camera, {
     type: 'pattern',
-    patternUrl: 'data/pattern-seed.patt',
+    patternUrl: 'data/pattern-prism.patt',
     changeMatrixMode: 'cameraTransformMatrix'
 });
 
-
+const markerPlane = new THREE.Mesh(
+    new THREE.PlaneBufferGeometry(1, 1),
+    new THREE.MeshBasicMaterial({
+        colorWrite: false,
+        depthWrite: false,
+    })
+);
+markerPlane.rotation.x = -0.5 * Math.PI;
 
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
 scene.add(ambientLight);
@@ -253,27 +185,67 @@ const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
 directionalLight.position.set(1, 1, 1);
 scene.add(directionalLight);
 
+const raycaster = new THREE.Raycaster();
+renderer.domElement.addEventListener('touchstart', (event) => {
+    event.preventDefault();
+    const touch = event.touches[0];
+    const element = event.currentTarget;
+    const x = touch.clientX - element.offsetLeft;
+    const y = touch.clientY - element.offsetTop;
+    const w = element.offsetWidth;
+    const h = element.offsetHeight;
+    const mouse = new THREE.Vector2((x / w) * 2 - 1, -(y / h) * 2 + 1);
+    console.log(mouse);
+    raycaster.setFromCamera(mouse, camera);
 
-document.addEventListener('touchstart', function (e) {
-    const b = new Ball();
-    console.log("touched");
-    Balls.push(b);
-}, { passive: false });
+    for (i = 0; i < 5; i++) {
+        for (j = 0; j < 6; j++) {
+            const mesh = group.children[i * 6 + j];
+            const intersects = raycaster.intersectObject(mesh);
+            if (intersects.length !== 0) {
+                console.log(i * 6 + j);
+                Prisms[i * 6 + j].height_pre = 6;
+                Prisms[i * 6 + j].height_pre2 = 6;
 
+            }
+        }
+    }
+
+});
+
+let sec_pre = 0;
 // animation
 requestAnimationFrame(function animate() {
     requestAnimationFrame(animate);
 
-    const sec = performance.now() / 1000;
+    let sec = performance.now() / 1000 - sec_pre;
+    sec_pre += sec;
+    if (sec > 0.05) sec = 0.05;
 
     if (arToolkitSource.ready) {
         arToolkitContext.update(arToolkitSource.domElement);
         scene.visible = camera.visible;
 
         if (camera.visible) {
+            let sum = 0;
             for (i = 0; i < 5; i++) {
                 for (j = 0; j < 6; j++) {
-                    Prisms[i * 6 + j].height = (1.1 + Math.cos(sec + i * 0.3) * 0.5 + Math.sin(sec + j * 0.7) * 0.5) * 3;
+                    let h = Prisms[i * 6 + j].height_pre;
+                    const k = 0.999;
+                    const s = 0.5 * sec * sec;
+                    h += (Prisms[i * 6 + j].height_pre - Prisms[i * 6 + j].height_pre2) * k;
+                    h -= (Prisms[i * 6 + j].height_pre - Prisms[(i + 1) % 5 * 6 + j].height_pre) * s;
+                    h -= (Prisms[i * 6 + j].height_pre - Prisms[(i + 4) % 5 * 6 + j].height_pre) * s;
+                    h -= (Prisms[i * 6 + j].height_pre - Prisms[i % 5 * 6 + (j + 1) % 6].height_pre) * s;
+                    h -= (Prisms[i * 6 + j].height_pre - Prisms[i % 5 * 6 + (j + 5) % 6].height_pre) * s;
+
+                    Prisms[i * 6 + j].height = h;// (1.1 + Math.cos(sec + i * 0.3) * 0.5 + Math.sin(sec + j * 0.7) * 0.5) * 3;
+                    sum += h;
+                }
+            }
+            for (i = 0; i < 5; i++) {
+                for (j = 0; j < 6; j++) {
+                    Prisms[i * 6 + j].height -= (sum - totalheight) / 30 * 0.001;
                     Prisms[i * 6 + j].updateHeight();
                 }
             }
